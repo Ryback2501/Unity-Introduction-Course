0eb24cd5e739b89351044652f227480b69a3537b
David <deiturbe@gmail.com>
2017-04-07 00:26:25 +0200

    Double jump implemented.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     /// <summary>
-    /// Reference to the rigidbody of the player
+    /// Reference to the rigidbody of the player.
     /// </summary>
     public Rigidbody MyRigidbody;
 
     /// <summary>
-    /// The acceleration of the movement
+    /// The acceleration of the movement.
     /// </summary>
     public float Acceleration;
 
     /// <summary>
-    /// The initial vertical velocity of the jump
+    /// The initial vertical velocity of the jump.
     /// </summary>
     public float JumpStartVelocity;
 
     /// <summary>
-    /// The speed the avatar moves at
+    /// The speed the avatar moves at.
     /// </summary>
     public float MaxSpeed;
 
@@ -30,9 +30,14 @@ public class Move : MonoBehaviour
     public float MaxSlope;
 
     /// <summary>
-    /// If TRUE, the player is in contact with the ground;
+    /// If TRUE, the player is in contact with the ground.
     /// </summary>
     private bool grounded;
+
+    /// <summary>
+    /// If TRUE, the player already preformed a double jump.
+    /// </summary>
+    private bool doubleJumped;
 	
 	// Update is called once per frame
 	void Update ()
@@ -59,8 +64,10 @@ public class Move : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (grounded)
+            if (grounded || !doubleJumped)
             {
+                doubleJumped = !grounded;
+
                 var velocity = MyRigidbody.velocity;
                 velocity.y = JumpStartVelocity;
                 MyRigidbody.velocity = velocity;
@@ -75,6 +82,7 @@ public class Move : MonoBehaviour
             if (Vector3.Angle(contact.normal, Vector3.up) < MaxSlope)
             {
                 grounded = true;
+                return;
             }
         }
     }
