6400c55ad05856f73534e0c84871122b3ac70c0a
David <deiturbe@gmail.com>
2017-04-10 13:53:36 +0200

    Shooting system added. Added Gun script to shoot bullets. Added a prefab, a script and a physic material for the Bullet Modified physics to prevent the Player to get hit by its bullets. Also, the player is now a Prefab too.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    /// <summary>
+    /// The speed the bullet moves at
+    /// </summary>
+    public float Speed;
+
+    /// <summary>
+    /// The maximum tyime the bullet can exist
+    /// </summary>
+    public float LifeTime;
+
+    /// <summary>
+    /// If TRUE, the bullet bounces with the surface it touches, otherwise, it it destroyed
+    /// </summary>
+    public bool Bounce;
+
+    /// <summary>
+    /// The elapsed ime since the bullet was created
+    /// </summary>
+    private float elapsedTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * Speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime > LifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!Bounce)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
