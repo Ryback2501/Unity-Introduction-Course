4f196d1ffceed5f05f18e1a0ac197963ce8f8332
David <deiturbe@gmail.com>
2017-04-11 04:15:08 +0200

    Added Life and Injurer components. Life component added to Player and Turret prefabs. Injurer component added to Bullet and TurretBullet prefabs.

---
+++ Assets/Scripts/Injurer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Injurer : MonoBehaviour
+{
+    public int Damage;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var life = other.gameObject.GetComponent<Life>();
+        if(life == null)
+        {
+            return;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Life : MonoBehaviour
+{
+    /// <summary>
+    /// Maximum life points
+    /// </summary>
+    public int MaxLifePoints;
+
+    /// <summary>
+    /// The current Life Points
+    /// </summary>
+    private int lifePoints;
+
+    void Awake()
+    {
+        lifePoints = MaxLifePoints;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Heal(int healedPoints)
+    {
+        lifePoints = Mathf.Min(MaxLifePoints, lifePoints + healedPoints);
+    }
+
+    public void Injure(int damage)
+    {
+        lifePoints = Mathf.Max(0, lifePoints - damage);
+        if(lifePoints == 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        if(GetComponent<Move>() != null)
+        {
+            //TODO: Go to GAME OVER
+        }
+        Destroy(gameObject);
+    }
+}
