4d9368d0c1c210f768ec3bd0ad487ada46c8753d
David <deiturbe@gmail.com>
2017-04-11 13:10:41 +0200

    Added Vertical Door functionality. Both doors are now prefabs. Added animations for open and close state and animator controller. Added VerticalDoor script to open and close the door through Animations.
    Also, Injurer component now performs damage to life components.
    And Speed of Player's Bullet has been increased.

---
+++ Assets/Scripts/VerticalDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalDoor : MonoBehaviour
+{
+    private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Move>() != null)
+        {
+            animator.SetBool("PlayerOnRange", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<Move>() != null)
+        {
+            animator.SetBool("PlayerOnRange", false);
+        }
+    }
+}
