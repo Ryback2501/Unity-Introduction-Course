c315eba80c446cd4fa6a9b2a6d94c29b4b236ebe
David <deiturbe@gmail.com>
2017-04-11 14:44:34 +0200

    Added functionality to the Horizontal Door. Added Open and Close animations to Horizontal Door.controller Added Knob script to receive interactions from Player. Added Hand component to player to enable it to interact with Horizontal Door. Level references set to make everything work.

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public float Reach;
+
+    public Transform Camera;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.F))
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.position, Camera.forward, out hitInfo, Reach))
+            {
+                var knob = hitInfo.transform.GetComponent<Knob>();
+                if(knob == null)
+                {
+                    return;
+                }
+                knob.Open();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Knob.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knob : MonoBehaviour
+{
+    public GameObject Turret;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Open()
+    {
+        if(Turret == null)
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+}
